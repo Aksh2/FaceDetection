f72fd6f140ec1026bfb1ca3ebfe2f3383ba8c22f
Akshay Kumar H <akshay.h@pharmeasy.in>
2021-08-30 21:36:34 +0530

    feature: added hilt di.

--- app/src/main/java/com/assignment/facedetection/FaceDetectionApp.kt
+++ app/src/main/java/com/assignment/facedetection/FaceDetectionApp.kt
@@ -3,7 +3,9 @@ package com.assignment.facedetection
 import android.app.Application
 import androidx.camera.camera2.Camera2Config
 import androidx.camera.core.CameraXConfig
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class FaceDetectionApp: Application(), CameraXConfig.Provider {
 
     override fun getCameraXConfig(): CameraXConfig {
